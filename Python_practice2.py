d2e593efb894356a2f05df3a175bb4caad4586da
Taylor Wessel <t.j.wessel@Taylors-MacBook-Pro.local>
2021-11-08 23:52:47 -0500

    Adding PyPoll.py file.

---
+++ Python_practice2.py
@@ -0,0 +1,14 @@
+count = 7
+while count < 5:
+    print("Hello World")
+
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+
+#Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+
+# Print the present time.
+print("The time right now is ", now)
+
